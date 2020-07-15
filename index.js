cbb34b4dd4f545385254d90d2464ad4f90e72fa3
dragonblitz10 <developer@dragondev.party>
2020-07-15 05:32:58 +0100

    version, status and general cleanup

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 const DankClient = require('./lib/DankClient');
-function Dank(token) {
-	return new dank(token);
-}
-
-Dank.Memer = DankClient;
-Dank.version = require('./package.json').version;
-
-module.exports = DankClient;
+module.exports = {
+	DankClient
+};
